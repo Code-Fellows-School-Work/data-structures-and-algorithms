41b61cfa53a7853585ddf1bfc06037a544ce55e7
Errol Vidad <errolvidad@gmail.com>
2023-12-11 23:11:42 +0000

    solved challenge 3

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -33,7 +33,7 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 
 const findHappiness = (arr) => {
   // Solution code here...
-  return arr.map((str) => str.includes(':)'));
+  return arr.filter((str) => str.includes(':)'));
 };
 
 /* ------------------------------------------------------------------------------------------------
