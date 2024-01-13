936652834208e994b6d7c9918ead219208dfcd11
evidad <errolvidad@gmail.com>
2024-01-12 22:44:58 -0700

    troubleshooting commit

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,29 @@
+# Used JB's class code to help write below code
+class Node:
+    def __init__(self, value):
+        self.value = value
+
 class LinkedList:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        def __init__(self):
+            self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+            return False
 
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
 
 class TargetError:
     pass
