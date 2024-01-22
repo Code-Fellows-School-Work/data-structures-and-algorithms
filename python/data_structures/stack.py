59a6a4cb4298f523bc2be31320a7b264a8407a87
evidad <errolvidad@gmail.com>
2024-01-22 08:41:52 -0800

    updated readme

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,4 +1,10 @@
 # class Node, Class Stack, __init__, push, pop, is_empty methods created in class with Adam as the instructor
+
+# used ChatGPT to help write error class
+class InvalidOperationError(Exception):
+        def __init__(self, message="Method not allowed on empty collection"):
+            super().__init__(message)
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
@@ -40,13 +46,16 @@ class Stack:
     def is_empty(self):
         if self.top == None:
             return True
-        else:
-            return "Method not allowed on empty collection"
-    
+           
     def peek(self):
-        """Peek will only review the node at the top of the stack.
-        Method checks the value of the node on the top and returns that value
-        """
-        value = self.top.value
-        return value
+        # """Peek will only review the node at the top of the stack.
+        # Method checks the value of the node on the top and returns that value
+        # """
+        if self.is_empty():
+            raise InvalidOperationError()
+        return self.top.value
+        # else:
+        #     value = self.top.value
+        #     return value
         
+    
