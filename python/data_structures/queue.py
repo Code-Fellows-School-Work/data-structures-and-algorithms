ac026371c87f659b55b22cef51d4fe5a36a3e90b
evidad <errolvidad@gmail.com>
2024-01-22 08:10:09 -0800

    baseline lab starter code

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,42 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class Queue:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        # if the queue is empty
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+        # queue isn't empty
+        else:
+            self.rear.next = new_node
+            # point the queue's self.rear to our new node. Update the pointer!
+            self.rear = new_node
+    
+    def dequeue(self):
+        if self.front is None:
+            # TODO: raise an error
+            return None
+        # get the value
+        dequeue_value = self.front.value
+
+        # move the front pointer to its next
+        self.front = self.front.next
+        # the Queue has become empty
+        if self.rear is None:
+            # also need to update self.rear
+            self.rear = None
+
+        return dequeue_value
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,42 @@
+# class Node, Class Stack, __init__, push, pop, is_empty methods created in class with Adam as the instructor
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class Stack:
     """
-    Put docstring here
+    Pushes a new value onto the top of the stack.
+    Removes the top value from the stack and returns it.
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+
+    def push(self, value):
+        # creates a new node
+        new_node = Node(value)
+        # point the new node to the current top
+        new_node.next = self.top
+        # reassign the self.top in the stack
+        self.top = new_node
+
+    def pop(self):
+        # popping from an empty list
+        # TODO: change this to raise an error
+        if self.top is None:
+            return None
+            # raise InvalidOperationError("Method not allowed on an empty collection")
+        
+        # get the return value
+        pop_value = self.top.value
+
+        # move the pointer which "removes" the node from the Stack
+        self.top = self.top.next
+
+        return pop_value
+    
+    def is_empty(self):
+        if self.top == None:
+            return True
