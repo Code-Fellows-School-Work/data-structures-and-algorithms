140097345925c743e2e8a1b2170f14edc5c39a0b
evidad <errolvidad@gmail.com>
2024-01-23 20:34:21 -0800

    added class for animal shelter, dog and cat objects

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,51 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    """
+    Instantiates an instance of an animal shelter
+
+    param: dog or cat object
+    """
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, name, species):
+
+        new_cat = Cat(name, species)
+        new_dog = Dog(name, species)
+
+        if self.rear is None:
+            self.rear = new_cat or new_dog
+            self.front = new_cat or new_dog
+
+        else:
+            self.rear.next = new_cat or new_dog
+            self.rear = new_cat or new_dog
+
+    def dequeue():
+        pass
 
 
 class Dog:
-    pass
+    """
+    Instantiates an instance of a dog object
+
+    param: name(string)
+    """
+    def __init__(self, name, next=None):
+        self.species = "dog"
+        self.next = next
+        self.name = name
 
 
 class Cat:
-    pass
+    """
+    Instantiates an instance of a cat object
+
+    param: name(string)
+    """
+    def __init__(self, name, next=None):
+        self.species = "cat"
+        self.next = next
+        self.name = name
