64aac7e8c22bc678771ca32d3c163a3b5650bd5a
evidad <errolvidad@gmail.com>
2024-01-24 21:02:40 -0800

    wrote validation functionality and passed all tests

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,32 @@
 from data_structures.queue import Queue
 
+# Used ChatGPT to help outline this code
+def multi_bracket_validation(string):
+    stack = []
 
-def multi_bracket_validation():
-    pass
+    for character in string:
+        if character == "(" or character == "[" or character == "{":
+            stack.append(character)
+        elif character == ")" or character == "]" or character == "}":
+            if len(stack) == 0:
+                return False
+            
+            top_of_stack = stack.pop()
+
+            if not compare(top_of_stack, character):
+                return False
+        
+    if len(stack) != 0:
+        return False
+    
+    return True
+    
+
+def compare(opening, closing):
+    if opening == '(' and closing == ')':
+        return True
+    if opening == '[' and closing == ']':
+        return True
+    if opening == '{' and closing == '}':
+        return True
+    return False
