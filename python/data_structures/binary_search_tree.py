dcb0abdc0fc86c91e2d34bb0916e3263f9b29e39
evidad <errolvidad@gmail.com>
2024-01-28 16:31:13 -0800

    passed all tests

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,59 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
     """
-    Put docstring here
+    A super class of the Binary Tree that add values and searches for the existence of a value
     """
-
+    # Either do this, or get rid of it because init is being conducted in sub class binarytree
     def __init__(self):
         # initialization here
-        pass
+        super().__init__()
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+
+        node = Node(value)
+        # covers edge case if root is None
+        if self.root is None:
+            self.root = node
+            return
+
+        def walk(node, node_to_add):
+            """
+                10 want to add 5
+                5 is less than 10 so need to add left
+
+            """
+
+            if node is None:
+                return
+
+            if node_to_add.value < node.value:
+                if node.left is None:
+                    node.left = node_to_add
+                    return
+                else:
+                    walk(node.left, node_to_add)
+                    return
+            elif node_to_add.value >= node.value: # here is node_to_add > node.value
+                if node.right is None:
+                    node.right = node_to_add
+                    return
+                else:
+                    walk(node.right, node_to_add)
+                    return
+        
+        walk(self.root, node)
+
+    def contains(self, value):
+        def search(node):
+            if node is None:
+                return False
+            if node.value == value:
+                return True
+            elif value < node.value:
+                return search(node.left)
+            else:
+                return search(node.right)
+
+        return search(self.root)
