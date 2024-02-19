1e7d0574a3af1bdc02b0a8aef730b794c5d2e12a
evidad <errolvidad@gmail.com>
2024-02-18 19:50:27 -0800

    added hash function

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -3,10 +3,13 @@ class Hashtable:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, size=1024):
+        self.size = size
 
-    def some_method(self):
-        # method body here
-        pass
+    def hash(self, key):
+        index = 0
+        for char in key:
+            index += ord(char)
+        index *= 599
+        index = index % 1024
+        return index
