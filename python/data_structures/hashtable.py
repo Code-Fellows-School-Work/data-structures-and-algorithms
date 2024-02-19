91cace7f397159c1301dcc237f630955ed5dda25
evidad <errolvidad@gmail.com>
2024-02-18 20:02:04 -0800

    updated init method

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -5,11 +5,28 @@ class Hashtable:
 
     def __init__(self, size=1024):
         self.size = size
+        self._buckets = [None] * size # creates a list of Nones
 
-    def hash(self, key):
+    def _hash(self, key): # the underscore before the method name is a python naming convention that conveys this method is not for public use
+        """
+        Internal use only. Converts a string to a index value to be stored in a hash table
+        """
         index = 0
         for char in key:
             index += ord(char)
         index *= 599
-        index = index % 1024
+        index = index % self.size
         return index
+    
+    def set(self, key, value):
+        pass
+
+    def get(self):
+        pass
+
+    def has(self):
+        pass
+
+    def keys(self):
+        pass
+
