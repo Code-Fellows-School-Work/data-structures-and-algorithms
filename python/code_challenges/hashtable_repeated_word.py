7aa818c8b124758fdf3add2951fe5182eb32a15e
evidad <errolvidad@gmail.com>
2024-02-19 19:44:07 -0800

    finished code challenge

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,10 @@
 from data_structures.hashtable import Hashtable
 
 
-def first_repeated_word():
-    pass
+def first_repeated_word(string):
+    reviewed_words = set()
+    for word in string.lower().split():
+        if word in reviewed_words:
+            return word
+
+        reviewed_words.add(word)
